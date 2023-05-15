650b85009a5c379496176eff53f43c0ee574c1d1
Yoshiyuki Kitaguchi <kitaguchi@yk-gankeisei.com>
2023-05-16 00:00:25 +0900

    Initial Commit

---
+++ BlepharitisApp/BlepharitisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlepharitisAppApp.swift
+//  BlepharitisApp
+//
+//  Created by Yoshiyuki Kitaguchi on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct BlepharitisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
